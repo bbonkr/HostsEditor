3b99657e78ab9edc78e1563738b115a5e13ea161
bbonkr <bbon@live.com>
2015-02-05 18:29:44 +0900

    add files

---
+++ src/HostsEditor/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace HostsEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.useBackupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.mainTextBox = new System.Windows.Forms.RichTextBox();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.wordWrapToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openBackupFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.toolToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "mainMenu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reloadToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // reloadToolStripMenuItem
+            // 
+            this.reloadToolStripMenuItem.Name = "reloadToolStripMenuItem";
+            this.reloadToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.reloadToolStripMenuItem.Text = "&Reload";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.useBackupToolStripMenuItem,
+            this.wordWrapToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "&Edit";
+            // 
+            // useBackupToolStripMenuItem
+            // 
+            this.useBackupToolStripMenuItem.Name = "useBackupToolStripMenuItem";
+            this.useBackupToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.useBackupToolStripMenuItem.Text = "Use &Backup";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 239);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "mainStatus";
+            // 
+            // mainTextBox
+            // 
+            this.mainTextBox.AcceptsTab = true;
+            this.mainTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.mainTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTextBox.EnableAutoDragDrop = true;
+            this.mainTextBox.HideSelection = false;
+            this.mainTextBox.Location = new System.Drawing.Point(0, 24);
+            this.mainTextBox.Name = "mainTextBox";
+            this.mainTextBox.Size = new System.Drawing.Size(284, 215);
+            this.mainTextBox.TabIndex = 2;
+            this.mainTextBox.Text = "";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel.Text = "toolStripStatusLabel1";
+            // 
+            // wordWrapToolStripMenuItem
+            // 
+            this.wordWrapToolStripMenuItem.Checked = true;
+            this.wordWrapToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.wordWrapToolStripMenuItem.Name = "wordWrapToolStripMenuItem";
+            this.wordWrapToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.wordWrapToolStripMenuItem.Text = "Word Wrap";
+            // 
+            // toolToolStripMenuItem
+            // 
+            this.toolToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openBackupFolderToolStripMenuItem});
+            this.toolToolStripMenuItem.Name = "toolToolStripMenuItem";
+            this.toolToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.toolToolStripMenuItem.Text = "Tool";
+            // 
+            // openBackupFolderToolStripMenuItem
+            // 
+            this.openBackupFolderToolStripMenuItem.Name = "openBackupFolderToolStripMenuItem";
+            this.openBackupFolderToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.openBackupFolderToolStripMenuItem.Text = "Open Backup Folder";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.mainTextBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "Hosts Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reloadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem useBackupToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox mainTextBox;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolStripMenuItem wordWrapToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openBackupFolderToolStripMenuItem;
+    }
+}
+
---
+++ src/HostsEditor/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HostsEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Setup();
+
+            this.Load += (s, e) =>
+            {
+                try
+                {
+                    this.LoadContent();
+                }
+                catch (Exception ex)
+                {
+                    // log
+                    this.Log(ex);
+                }
+            };
+
+            this.mainTextBox.TextChanged += (s, e) =>
+            {
+                //this.SetTextColor();
+                this.ContentChanged = true;
+                this.SetStatusLabel("Modified");
+            };
+
+            this.saveToolStripMenuItem.Click += (s, e) => { RunSave(); };
+
+            this.exitToolStripMenuItem.Click += (s, e) => { this.Close(); };
+
+            this.reloadToolStripMenuItem.Click += (s, e) => { this.LoadContent(); };
+
+            this.FormClosing += (s, e) =>
+            {
+                if (this.ContentChanged)
+                {
+                    DialogResult dialogResult = MessageBox.Show("The content was modified. Do want to cancel the modification?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (DialogResult.Yes != dialogResult)
+                    {
+                        e.Cancel = true;
+                    }
+                }
+            };
+
+            this.wordWrapToolStripMenuItem.Click += (s, e) =>
+            {
+                ToolStripMenuItem thisControl = (ToolStripMenuItem)s;
+                thisControl.Checked = !thisControl.Checked;
+                Properties.Settings.Default.WordWrap = thisControl.Checked;
+                Properties.Settings.Default.Save();
+            };
+
+            this.wordWrapToolStripMenuItem.CheckedChanged += (s, e) =>
+            {
+                ToolStripMenuItem thisControl = (ToolStripMenuItem)s;
+                this.mainTextBox.WordWrap = thisControl.Checked;
+            };
+
+            this.useBackupToolStripMenuItem.Click += (s, e) =>
+            {
+                ToolStripMenuItem thisControl = (ToolStripMenuItem)s;
+                thisControl.Checked = !thisControl.Checked;
+                Properties.Settings.Default.UseBackup = thisControl.Checked;
+                Properties.Settings.Default.Save();
+            };
+
+            this.openBackupFolderToolStripMenuItem.Click += (s, e) =>
+            {
+                string strBackupPath = this.GetBackupPath();
+                if (!Directory.Exists(strBackupPath))
+                {
+                    Directory.CreateDirectory(strBackupPath);
+                }
+
+                Process.Start(strBackupPath);
+            };
+        }
+
+        private void Setup()
+        {
+            this.Size = new Size(600, 480);
+            Properties.Settings.Default.Reload();
+
+            this.wordWrapToolStripMenuItem.Checked = Properties.Settings.Default.WordWrap;
+            this.useBackupToolStripMenuItem.Checked = Properties.Settings.Default.UseBackup;
+
+            this.Font = new Font(System.Drawing.SystemFonts.DefaultFont, FontStyle.Regular);
+            this.mainTextBox.WordWrap = this.wordWrapToolStripMenuItem.Checked;
+        }
+
+        private void Log(object obj)
+        {
+            // Do nothing.
+        }
+
+        private void LoadContent()
+        {
+            string strFilepath = this.GetFilepath();
+            //FileInfo hostsFile = new FileInfo(strFilepath);
+            // Remove Tab char
+            string strContent = File.ReadAllText(strFilepath);
+            //if (!string.IsNullOrEmpty(strContent)) { strContent = strContent.Replace("\t", "".PadLeft(4, ' ')); }
+            this.mainTextBox.Text = strContent;
+
+            //this.SetTextColor();
+            this.ContentChanged = false;
+            this.SetStatusLabel("Ready");
+        }
+
+        private void SetTextColor()
+        {
+            int nCurrentIndex = this.mainTextBox.SelectionStart;
+            Color commentColor = Color.DarkGreen;
+            string strNewLine = System.Environment.NewLine;
+
+            int nIndex = 0;
+            int nLineLength = 0;
+
+            this.mainTextBox.AutoWordSelection = false;
+
+            foreach (string str in this.mainTextBox.Lines)
+            {
+                nLineLength = str.Length;
+
+                if (str.StartsWith("#"))
+                {
+                    this.mainTextBox.Select(nIndex, nLineLength);
+                    this.mainTextBox.SelectionColor = commentColor;
+                }
+
+                nIndex += (nLineLength - 1 + strNewLine.Length);
+            }
+
+            this.mainTextBox.Select(nCurrentIndex, 0);
+        }
+
+        private string GetFilepath()
+        {
+            string strWindowsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Windows);
+            string strFilepath = string.Format(@"{0}\system32\drivers\etc\hosts", strWindowsPath);
+            return strFilepath;
+        }
+
+        private string GetBackupPath()
+        {
+            string strApplicaionDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string strBackupPath = string.Format(@"{0}\{1}\{2}\backup", strApplicaionDataPath, this.CompanyName, this.ProductName);
+            return strBackupPath;
+        }
+
+        private bool Backup(out string message)
+        {
+            message = string.Empty;
+            // ****************************************************
+            // Backup file format
+            // hosts-yyyy-MM-dd-nnnnn
+            // ****************************************************
+            string strBackupFilePrefix = "hosts";
+            int nMaxWhileLoop = 10000;
+            int nWhileLoop = 0;
+            string strBackupPath = this.GetBackupPath();
+
+            if (!Directory.Exists(strBackupPath)) { Directory.CreateDirectory(strBackupPath); }
+
+            string[] files = Directory.GetFiles(strBackupPath, string.Format("{0}-{1:yyyy-MM-dd}*", strBackupFilePrefix, DateTime.Now));
+
+            int nFileCount = files.Length;
+            string strBackupFilename = string.Empty;
+            int maxSeq = 0;
+
+            while (true)
+            {
+                if (nFileCount > 0)
+                {
+                    string strLastFile = files.OrderByDescending(f => f.Substring(f.Length - 5, 5)).First();
+                    int.TryParse(strLastFile.Substring(strLastFile.Length - 5, 5), out maxSeq);
+                }
+
+                nFileCount++;
+                maxSeq++;
+
+                strBackupFilename = string.Format("{0}\\{1}-{2:yyyy-MM-dd}-{3}", strBackupPath, strBackupFilePrefix, DateTime.Now, maxSeq.ToString().PadLeft(5, '0'));
+                if (!File.Exists(strBackupFilename))
+                {
+                    File.Copy(this.GetFilepath(), strBackupFilename);
+                    if (File.Exists(strBackupFilename)) { break; }
+                }
+
+                nWhileLoop++;
+                if (nWhileLoop > nMaxWhileLoop)
+                {
+                    message = string.Format("Try {0:n0} times, but could not save backup file.", nWhileLoop);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool Save(out string message)
+        {
+            message = string.Empty;
+            string strFilepath = this.GetFilepath();
+            try
+            {
+                if (this.useBackupToolStripMenuItem.Checked)
+                {
+                    if (!this.Backup(out message)) { throw new Exception(message); }
+                }
+                File.WriteAllText(strFilepath, this.mainTextBox.Text);
+                this.ContentChanged = false;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                this.Log(ex);
+                return false;
+            }
+        }
+
+        private void RunSave()
+        {
+            string strMessage = string.Empty;
+            //DialogResult dialogResult = MessageBox.Show("Do you want to Save hosts file?\r\n* Caution Overwrite it.", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            //if (DialogResult.Yes == dialogResult)
+            //{
+            //    if (this.Save(out strMessage))
+            //    {
+            //        // saved
+            //        this.SetStatusLabel("Saved.");
+            //    }
+            //    else
+            //    {
+            //        // Could not save
+            //        this.SetStatusLabel("Could not save the file.");
+            //    }
+            //}
+
+            // I don't want to confirm windows.
+            if (this.Save(out strMessage))
+            {
+                // saved
+                this.SetStatusLabel("Saved.");
+            }
+            else
+            {
+                // Could not save
+                this.SetStatusLabel("Could not save the file.");
+            }
+        }
+
+        private void SetStatusLabel(string message)
+        {
+            this.toolStripStatusLabel.Text = message;
+        }
+
+        private void CanSave()
+        {
+            this.saveToolStripMenuItem.Enabled = this.ContentChanged;
+        }
+
+        private bool _TextChanged = false;
+
+        private bool ContentChanged
+        {
+            set
+            {
+                this._TextChanged = value;
+                this.CanSave();
+            }
+            get { return this._TextChanged; }
+        }
+    }
+}
